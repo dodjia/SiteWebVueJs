70bb269000c43f3228eb93516d2d4e5cd8303123
Dodjia <fillingerhugo@gmail.com>
2020-07-23 18:55:16 +0200

    Avancement sur la page d'accueil

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="infos">
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      La psychologie et le développement personnel est un choix de vie pour moi. Après un parcours professionnel dans de multiples domaines,
+      j’ai choisi de reprendre mes études à l’âge de 34  ans par vocation. L’humain est pour moi une véritable source d’inspiration et ma passion est de l’accompagner autant qu’il m’est possible,
+      vers la prise de conscience de ses potentiels et de ses multiples possibilités.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,108 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div class="prenom">
+            <p class="d-flex justify-content-center">Estelle Lesein </p>
+            <p class="d-flex justify-content-center profession">Psychologue clinicienne</p>
+        </div>
+        <div class="infos d-flex justify-content-center">
+            <img class="profilImage" src="../assets/images/pp.jpg" alt="photo">
+            <div class="infoText">
+                <div class="quisuisje">
+                    Qui suis-je ?
+                </div>
+                La psychologie et le développement personnel est un choix de vie pour moi. Après un parcours
+                professionnel dans de multiples domaines,
+                j’ai choisi de reprendre mes études à l’âge de 34 ans par vocation. L’humain est pour moi une véritable
+                source d’inspiration et ma passion est de l’accompagner autant qu’il m’est possible,
+                vers la prise de conscience de ses potentiels et de ses multiples possibilités.
+            </div>
+        </div>
+        <div class="prenom d-flex justify-content-center contact">
+            <div class="infoText">
+                <div>
+                    Me contacter:
+                </div>
+                <div class="marginTopPrenom">
+                    Vous pouvez me contacter par téléphone au: 07 86 57 79 45
+                </div>
+                <div class="marginTopPrenom">
+                    Par mail: EstelleLesein@gmail.com
+                </div>
+                <div class="marginTopPrenom">
+                    Ou :
+                </div>
+                <div class="d-flex justify-content-center">
+
+                    <div class="rdvButton">
+                        <div  class="marginauto">
+                            <a class="link" href="https://www.doctolib.fr/psychologue/rixheim/estelle-lesein-ulmann"
+                               target="_blank"> Prendre rendez-vous directement en ligne </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
+<style lang="scss">
+    .marginTopPrenom {
+        margin-top: 20px;
+    }
+
+    .profilImage {
+        width: 1250px;
+        float: left;
+    }
+
+    .quisuisje {
+        margin-bottom: 6px;
+    }
+
+    .infos {
+        border-collapse: collapse;
+        border: 1px solid white;
+        margin: auto;
+        width: 800px;
+        background-color: rgba(0, 0, 0, 0.8);
+        text-align: justify;
+        vertical-align: middle;
+        display: inline-block;
+        margin-bottom: 20px;
+    }
+
+    .infoText {
+        padding: 3%;
+        padding-left: 20px;
+
+    }
+
+    .link {
+        text-decoration: none;
+    }
+
+    .rdvButton {
+        margin-top: 35px;
+        background-color: rgba(61, 52, 52, 0.92);
+        width: 200px;
+        text-decoration: none;
+
+    }
+    .link:hover {
+        text-decoration: none;
+        color: white;
+    }
+    .marginauto {
+        margin: auto;
+    }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-import Swal from 'sweetalert2/dist/sweetalert2.js';
-import 'sweetalert2/src/sweetalert2.scss'
+    // @ is an alias to /src
+    import 'sweetalert2/src/sweetalert2.scss'
 
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  },
-  beforeMount() {
-    Swal.fire('Site web actuellement en construction')
-  }
-}
+    export default {
+        name: 'Home',
+    }
 </script>
